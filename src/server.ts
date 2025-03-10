aff0291b52dc4de085de9ca7f36d0762359e0d40
Ahmad Zaki <shaikhzaki8@gmail.com>
2025-03-10 20:15:04 +0530

    add files vercel.json

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
- import app from "./app";
+//  import app from "./app";
+import express from "express";
+ import cors from "cors";
  import { AppDataSource } from "./config/database";
+ import productRoutes from "./routes/productRoutes";
 
- const PORT = process.env.PORT || 5000;
+//  const PORT = process.env.PORT || 5000;
+const app = express();
 
- AppDataSource.initialize().then(() => {
-    console.log("Database connected");
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
-}).catch((error) => console.log(error));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("E-commerce Admin API is running 🚀");
+});
+
+app.use("/api/products", productRoutes);
+
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.error("Database connection error: ", err);
+});
+
+//  AppDataSource.initialize().then(() => {
+//     console.log("Database connected");
+//     app.listen(PORT, () => {
+//         console.log(`Server running on http://localhost:${PORT}`);
+//     });
+// }).catch((error) => console.log(error));
+
+export default app;
